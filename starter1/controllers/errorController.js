4de2ffa4e98efe279b7830429b4669264d6c57a0
ahmed.touti <ahmedtouti.r@gmail.com>
2022-09-30 11:03:12 +0100

    add err to prod MOD

--- starter1/controllers/errorController.js
+++ starter1/controllers/errorController.js
@@ -1,21 +1,22 @@
 const AppError = require('../utils/appError');
 
 const handleCastErrorDB = err => {
-  const message = `Invalid  ${err.path} => ${err.value}.`;
+  const message = `Invalid  this : [${err.path}] => [${err.value}].`;
   return new AppError(message, 400);
 };
 
 const handleDuplicateFieldsDB = err => {
-  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  const value = err.errmsg.match(/(["'])(?:(?=(\\?))\2.)*?\1/)[0];
   console.log(value);
 
-  const message = `Duplicate field value: ${value}. Please use another value!`;
+  const message = `Duplicate field value:  ${value} . Please use another value!`;
   return new AppError(message, 400);
 };
+
 const handleValidationErrorDB = err => {
   const errors = Object.values(err.errors).map(el => el.message);
 
-  const message = `Invalid input data. ${errors.join('. ')}`;
+  const message = `Invalid input data : ${errors.join(' , and ')}`;
   return new AppError(message, 400);
 };
 
@@ -28,6 +29,12 @@ const sendErrorDev = (err, res) => {
   });
 };
 
+const handleJWTError = () =>
+  new AppError('Invalid token . Please log in again!', 401);
+
+const handleJWTExpiredError = () =>
+  new AppError('Your token has expired . Please log in again!', 401);
+
 const sendErrorProd = (err, res) => {
   // Operational, trusted error: send message to client
   if (err.isOperational) {
@@ -39,7 +46,7 @@ const sendErrorProd = (err, res) => {
     // Programming or other unknown error: don't leak error details
   } else {
     // 1) Log error
-    console.error('ERROR 💥', err);
+    console.error('ERROR 💥 ----->', err);
 
     // 2) Send generic message
     res.status(500).json({
@@ -64,6 +71,8 @@ module.exports = (err, req, res, next) => {
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
     if (error.name === 'ValidationError')
       error = handleValidationErrorDB(error);
+    if (error.name === 'JsonWebTokenError') error = handleJWTError();
+    if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
 
     sendErrorProd(error, res);
   }
