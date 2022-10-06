6b082bb271ad8911639ad2d194f2b887b9dd6ae9
ahmed.touti <ahmedtouti.r@gmail.com>
2022-10-06 16:35:43 +0100

    add Reviews model Schema

--- starter1/models/tourModel.js
+++ starter1/models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+// const User = require('./userModel');
 // const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
@@ -77,16 +78,59 @@ const tourSchema = new mongoose.Schema(
     secretTour: {
       type: Boolean,
       default: false
-    }
+    },
+    startLocation: {
+      // GeoJSON
+      type: {
+        type: String,
+        default: 'Point',
+        enum: ['Point']
+      },
+      coordinates: [Number],
+      address: String,
+      description: String
+    },
+    locations: [
+      {
+        type: {
+          type: String,
+          default: 'Point',
+          enum: ['Point']
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number
+      }
+    ],
+    guides: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User'
+      }
+    ]
   },
+
   {
     toJSON: { virtuals: true },
     toObject: { virtuals: true }
   }
 );
 
+// tourSchema.index({ price: 1 });
+tourSchema.index({ price: 1, ratingsAverage: -1 });
+tourSchema.index({ slug: 1 });
+
+//
 tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
+});
+
+//
+tourSchema.virtual('reviews', {
+  ref: 'Review',
+  foreignField: 'tour',
+  localField: '_id'
 });
 
 // DOCUMENT MIDDLEWARE: runs before .save() and .create()
@@ -95,18 +139,12 @@ tourSchema.pre('save', function(next) {
   next();
 });
 
-// tourSchema.pre('save', function(next) {
-//   console.log('Will save document...');
-//   next();
-// });
-
-// tourSchema.post('save', function(doc, next) {
-//   console.log(doc);
-//   next();
+// tourSchema.pre('save', async function(next) {
+//   const guidesPromises = this.guides.map(async id => User.findById(id));
+//   this.guides = await Promise.all(guidesPromises);
 // });
 
 // QUERY MIDDLEWARE
-// tourSchema.pre('find', function(next) {
 tourSchema.pre(/^find/, function(next) {
   this.find({ secretTour: { $ne: true } });
 
@@ -114,8 +152,17 @@ tourSchema.pre(/^find/, function(next) {
   next();
 });
 
+// populate ==> ici afficher les donner de user sur postmen only (en ne peux pas voir sur dababase)
+tourSchema.pre(/^find/, function(next) {
+  this.populate({
+    path: 'guides',
+    select: '-__v -passwordChangedAt'
+  });
+  next();
+});
+
 tourSchema.post(/^find/, function(docs, next) {
-  console.log(`Query took ${Date.now() - this.start} milliseconds!`);
+  console.log(`💥 -----> Query took ${Date.now() - this.start} milliseconds!`);
   next();
 });
 
@@ -123,7 +170,7 @@ tourSchema.post(/^find/, function(docs, next) {
 tourSchema.pre('aggregate', function(next) {
   this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
 
-  console.log(this.pipeline());
+  console.log('this.pipeline() 💥 ----->', this.pipeline());
   next();
 });
 
