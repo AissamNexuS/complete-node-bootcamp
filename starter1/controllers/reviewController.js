8b32566583df1ca6784f53dd393bebc406a9d1b7
ahmed.touti <ahmedtouti.r@gmail.com>
2022-10-06 16:34:21 +0100

    add `review` controller

---
+++ starter1/controllers/reviewController.js
@@ -0,0 +1,15 @@
+const Review = require('../models/reviewModel');
+const factory = require('./hendlerFactory');
+
+exports.setTourUserIds = (req, res, next) => {
+  //allow nested routes
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user.id;
+  next();
+};
+
+exports.getReview = factory.getOne(Review);
+exports.getAllReviews = factory.getAll(Review);
+exports.createReview = factory.createOne(Review);
+exports.updateReview = factory.updateOne(Review);
+exports.deleteReview = factory.DeleteOne(Review);
